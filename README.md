02d8451ec66fe4865cbe0b44d6a4b6162d89e539
CatYuan <catherine.cj.yuan@gmail.com>
2020-07-02 10:21:27 -0400

    data fetching w/ hooks works

--- README.md
+++ README.md
@@ -10,4 +10,7 @@ To build for production, use `npm run build`
 
 The react-app is stored in the `/react-spa` folder.
 
-Data fetching w/ hooks in `/react-spa/src/app.js` is not fully implemented. To finish implementing, follow [this](https://www.robinwieruch.de/react-hooks-fetch-data) guide.
+Data fetching w/ hooks in `/react-spa/src/app.js` is implemented, following [this](https://www.robinwieruch.de/react-hooks-fetch-data) guide.
+
+In the future, this will be depreciated. Hooks are not supposed to be used for async data fetching. This functionality will be replaced by `<Suspense>`. For more info on this, check [here](https://reactjs.org/docs/concurrent-mode-suspense.html).
+In the experimental stage, here is a [guide](https://blog.logrocket.com/react-suspense-for-data-fetching/) that deals with async data fetching using `<Suspense>`
--- react-spa/src/App.js
+++ react-spa/src/App.js
@@ -1,53 +1,61 @@
 import React, { Component, useState, useEffect } from "react";
+import { useAsyncResource } from "use-async-resource";
 
 let DEV_URL = "";
 if (process.env.NODE_ENV === "development") {
   DEV_URL = "http://localhost:3000";
 }
 
-// const App = () => {
-//   const [users, setUsers] = useState([]);
+const promise = fetch(`${DEV_URL}/users`);
 
-//   useEffect(async () => {
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    promise
+      .then((data) => data.json())
+      .then((data_json) => {
+        setUsers(data_json);
+      });
+  }, []);
+
+  if (users.length == 0) return <p> loading users... </p>;
+
+  return (
+    <div className="App">
+      <p>Users: </p>
+      {users.map((user) => (
+        <p>{user}</p>
+      ))}
+    </div>
+  );
+};
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       users: [],
+//     };
+//   }
+
+//   async componentDidMount() {
 //     const res = await fetch(`${DEV_URL}/users`);
-//     setUsers(await res.json());
-//   });
+//     const users = await res.json();
+//     this.setState({ users });
+//   }
 
-//   return (
-//     <div className="App">
-//       <p>Users: </p>
-//       {users.map((user) => (
-//         <p>{user}</p>
-//       ))}
-//     </div>
-//   );
-// };
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      users: [],
-    };
-  }
-
-  async componentDidMount() {
-    const res = await fetch(`${DEV_URL}/users`);
-    const users = await res.json();
-    this.setState({ users });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <p>Users: </p>
-        {this.state.users.map((user) => (
-          <p>{user}</p>
-        ))}
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="App">
+//         <p>Users: </p>
+//         {this.state.users.map((user) => (
+//           <p>{user}</p>
+//         ))}
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
